49f1ea277b7925ee26dd8f8eb5ff028ec768912b
hyeokcheol <hyeokcheol@192.168.2.5>
2017-03-10 02:05:40 +0900

    1. spring boot 프로젝트 2. gradle 프로젝트

---
+++ src/main/java/com/peoplecode/PeoplecodeApplication.java
@@ -0,0 +1,12 @@
+package com.peoplecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplecodeApplication.class, args);
+	}
+}
