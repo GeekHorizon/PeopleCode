4c72a13aa05cfe9ca413d9e5084d90c32e8ed040
hyeokcheol <hyeokcheol@192.168.2.5>
2017-03-10 02:39:44 +0900

    TEST

--- src/main/java/com/peoplecode/PeoplecodeApplication.java
+++ src/main/java/com/peoplecode/PeoplecodeApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PeoplecodeApplication {
 
+
 	public static void main(String[] args) {
+		
+		
 		SpringApplication.run(PeoplecodeApplication.class, args);
 	}
 }
---
+++ src/main/java/com/peoplecode/blog/Hello.java
@@ -0,0 +1,5 @@
+package com.peoplecode.blog;
+
+public class Hello {
+
+}
